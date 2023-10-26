bc5ab6bb9fad76c0808b5c7b25a0ca87c8a20041
Gaspare Tocci <36007938+Riello76@users.noreply.github.com>
2023-10-26 18:06:03 +0100

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,11 +1,23 @@
-// Close all open paragraphs when a button is clicked
-var buttons = document.querySelectorAll('.btn-secondary');
-buttons.forEach(function (button) {
-    button.addEventListener('click', function () {
-        // Close all open paragraphs
-        var collapses = document.querySelectorAll('.collapse.show');
-        collapses.forEach(function (collapse) {
-            collapse.classList.remove('show');
+document.addEventListener('DOMContentLoaded', function() {
+    var buttons = ['profile', 'experience', 'education', 'skills', 'hobbies', 'about-me'];
+
+    buttons.forEach(function(buttonId) {
+        var button = document.getElementById(buttonId + '-btn');
+        var content = document.getElementById(buttonId);
+
+        button.addEventListener('click', function() {
+            var isVisible = content.style.display === 'block';
+            hideAll();
+            if (!isVisible) {
+                content.style.display = 'block';
+            }
         });
     });
+
+    function hideAll() {
+        buttons.forEach(function(buttonId) {
+            var content = document.getElementById(buttonId);
+            content.style.display = 'none';
+        });
+    }
 });
