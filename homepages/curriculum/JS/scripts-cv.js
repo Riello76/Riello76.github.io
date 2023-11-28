ec668c86d60e94e8628a3c914a334506ee55a324
Gaspare Tocci <36007938+Riello76@users.noreply.github.com>
2023-11-28 11:09:50 +0000

    Update scripts-cv.js

--- homepages/curriculum/JS/scripts-cv.js
+++ homepages/curriculum/JS/scripts-cv.js
@@ -20,8 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
             content.style.display = 'none';
         });
     }
-});
-document.addEventListener('DOMContentLoaded', function() {
+
     var curriculumBtn = document.getElementById('curriculum-btn');
     var btnGroup = document.querySelector('.btn-group');
 
@@ -29,36 +28,38 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault(); 
         btnGroup.classList.toggle('show-buttons'); 
     });
-});
-document.getElementById('menu-btn').addEventListener('click', function() {
-    toggleButtonsVisibility();
-});
 
-document.getElementById('curriculum-btn').addEventListener('click', function() {
-    toggleButtonsVisibility();
-});
+    document.getElementById('menu-btn').addEventListener('click', function() {
+        toggleButtonsVisibility();
+    });
 
-document.getElementById('projects-btn').addEventListener('click', function() {
-    toggleButtonsVisibility();
-});
+    document.getElementById('curriculum-btn').addEventListener('click', function() {
+        toggleButtonsVisibility();
+    });
 
-document.getElementById('home-btn-back').addEventListener('click', function() {
-    toggleButtonsVisibility();
-});
+    document.getElementById('projects-btn').addEventListener('click', function() {
+        toggleButtonsVisibility();
+    });
 
-function toggleButtonsVisibility() {
-    var curriculumBtn = document.getElementById('curriculum-btn');
-    var projectsBtn = document.getElementById('projects-btn');
-    var homeBtnBack = document.getElementById('home-btn-back');
-    
-    if (curriculumBtn.classList.contains('hidden')) {
-        curriculumBtn.classList.remove('hidden');
-        projectsBtn.classList.remove('hidden');
-        homeBtnBack.classList.remove('hidden');
-    } else {
-        curriculumBtn.classList.add('hidden');
-        projectsBtn.classList.add('hidden');
-        homeBtnBack.classList.add('hidden');
+    document.getElementById('home-btn-back').addEventListener('click', function() {
+        toggleButtonsVisibility();
+    });
+
+    function toggleButtonsVisibility() {
+        var curriculumBtn = document.getElementById('curriculum-btn');
+        var projectsBtn = document.getElementById('projects-btn');
+        var homeBtnBack = document.getElementById('home-btn-back');
+        
+        if (curriculumBtn.classList.contains('hidden')) {
+            curriculumBtn.classList.remove('hidden');
+            projectsBtn.classList.remove('hidden');
+            homeBtnBack.classList.remove('hidden');
+        } else {
+            curriculumBtn.classList.add('hidden');
+            projectsBtn.classList.add('hidden');
+            homeBtnBack.classList.add('hidden');
+        }
     }
-}
-
+    
+    hideAll(); // Chiude automaticamente le finestre al caricamento della pagina
+});
