d677254e07adcd1cd2cb6fcd9c1c123be2de9e0f
Gaspare Tocci <36007938+Riello76@users.noreply.github.com>
2023-11-28 17:27:42 +0000

    Update scripts-cv.js

--- homepages/curriculum/JS/scripts-cv.js
+++ homepages/curriculum/JS/scripts-cv.js
@@ -29,37 +29,4 @@ document.addEventListener('DOMContentLoaded', function() {
         btnGroup.classList.toggle('show-buttons'); 
     });
 
-    document.getElementById('menu-btn').addEventListener('click', function() {
-        toggleButtonsVisibility();
-    });
-
-    document.getElementById('curriculum-btn').addEventListener('click', function() {
-        toggleButtonsVisibility();
-    });
-
-    document.getElementById('projects-btn').addEventListener('click', function() {
-        toggleButtonsVisibility();
-    });
-
-    document.getElementById('home-btn-back').addEventListener('click', function() {
-        toggleButtonsVisibility();
-    });
-
-    function toggleButtonsVisibility() {
-        var curriculumBtn = document.getElementById('curriculum-btn');
-        var projectsBtn = document.getElementById('projects-btn');
-        var homeBtnBack = document.getElementById('home-btn-back');
-        
-        if (curriculumBtn.classList.contains('hidden')) {
-            curriculumBtn.classList.remove('hidden');
-            projectsBtn.classList.remove('hidden');
-            homeBtnBack.classList.remove('hidden');
-        } else {
-            curriculumBtn.classList.add('hidden');
-            projectsBtn.classList.add('hidden');
-            homeBtnBack.classList.add('hidden');
-        }
-    }
     
-    hideAll(); // Chiude automaticamente le finestre al caricamento della pagina
-});
